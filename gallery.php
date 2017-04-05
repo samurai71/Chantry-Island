b56605fb8552834c5f6e2cd9f0319f09c51ddc03
marklanderyou <marklanderyou@macbookpro.fc.ca>
2017-04-05 11:58:16 -0400

    udpate

--- gallery.php
+++ gallery.php
@@ -35,26 +35,26 @@
     <h2 class="text-center">Gallery</h2>
 
     <ul class="small-block-grid-2 medium-block-grid-6 large-block-grid-9">
-      <li><a href="gallery/1.jpg" data-lightbox="gallery"><img src="gallery/Thumbs/Pic_01_TH.jpg" alt=""></a></li>
-      <li><a href="gallery/2.jpg" data-lightbox="gallery"><img src="gallery/Thumbs/Pic_02_TH.jpg" alt=""></a></li>
-      <li><img src="gallery/Thumbs/Pic_03_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_04_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_05_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_06_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_07_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_08_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_09_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_10_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_11_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_12_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_13_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_14_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_15_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_16_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_17_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_18_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_19_TH.jpg" alt=""></li>
-      <li><img src="gallery/Thumbs/Pic_20_TH.jpg" alt=""></li>
+      <li id="1"><a href="gallery/1.jpg" data-lightbox="gallery"><img src="gallery/Thumbs/Pic_01_TH.jpg" alt=""></a></li>
+      <li id="2"><a href="gallery/2.jpg" data-lightbox="gallery"><img src="gallery/Thumbs/Pic_02_TH.jpg" alt=""></a></li>
+      <li id="3"><img src="gallery/Thumbs/Pic_03_TH.jpg" alt=""></li>
+      <li id="4"><img src="gallery/Thumbs/Pic_04_TH.jpg" alt=""></li>
+      <li id="5"><img src="gallery/Thumbs/Pic_05_TH.jpg" alt=""></li>
+      <li id="6"><img src="gallery/Thumbs/Pic_06_TH.jpg" alt=""></li>
+      <li id="7"><img src="gallery/Thumbs/Pic_07_TH.jpg" alt=""></li>
+      <li id="8"><img src="gallery/Thumbs/Pic_08_TH.jpg" alt=""></li>
+      <li id="9"><img src="gallery/Thumbs/Pic_09_TH.jpg" alt=""></li>
+      <li id="10"><img src="gallery/Thumbs/Pic_10_TH.jpg" alt=""></li>
+      <li id="11"><img src="gallery/Thumbs/Pic_11_TH.jpg" alt=""></li>
+      <li id="12"><img src="gallery/Thumbs/Pic_12_TH.jpg" alt=""></li>
+      <li id="13"><img src="gallery/Thumbs/Pic_13_TH.jpg" alt=""></li>
+      <li id="14"><img src="gallery/Thumbs/Pic_14_TH.jpg" alt=""></li>
+      <li id="15"><img src="gallery/Thumbs/Pic_15_TH.jpg" alt=""></li>
+      <li id="16"><img src="gallery/Thumbs/Pic_16_TH.jpg" alt=""></li>
+      <li id="17"><img src="gallery/Thumbs/Pic_17_TH.jpg" alt=""></li>
+      <li id="18"><img src="gallery/Thumbs/Pic_18_TH.jpg" alt=""></li>
+      <li id="19"><img src="gallery/Thumbs/Pic_19_TH.jpg" alt=""></li>
+      <li id="20"><img src="gallery/Thumbs/Pic_20_TH.jpg" alt=""></li>
     </ul>
 
    
